b12f15a3a276a503e75145d26fcfe2e3892c0989
djs <lambdan@users.noreply.github.com>
2020-01-23 20:54:21 +0100

    handle more arguments and handle them better

--- no-intro-dat-matcher.py
+++ no-intro-dat-matcher.py
@@ -1,16 +1,59 @@
 import os, sys, hashlib, shutil, time, xmltodict
 from tqdm import tqdm
+from argparse import ArgumentParser
+
 
 large_size = 100*1000*1000 # 100 MB
 
-if len(sys.argv) < 3:
-    sys.exit('Usage: %s datfile.dat /folder/with/roms/' % sys.argv[0])
+# handle arguments
+parser = ArgumentParser(description='Matches files (typically ROMs) with hashes from No-Intro .dat files to properly rename the files (roms)')
+parser.add_argument("--input", action='store', dest='inputfolder', help='folder containing all roms', required=True)
+parser.add_argument("--dat", action='store', dest='datfile', help='path to .dat file', required=True)
+parser.add_argument("--output", action='store', dest='outputfolder', help='output folder for renamed roms. defaults to current working dir with the dat file as a subfolder if not specified')
+parser.add_argument("--mode", action='store', dest='workmode', help='copy (cp) or move (mv) roms. defaults to copy if not specified')
+parser.add_argument('--verbose', action="store_true", dest='verbose', default=False)
+parsed = parser.parse_args()
 
-inputdir = os.path.abspath(sys.argv[2])
-datfile = os.path.abspath(sys.argv[1])
+inputdir = parsed.inputfolder
+datfile = parsed.datfile
+verbose = parsed.verbose
+
+if verbose:
+	verbose = True
+	print("Verbose mode activated")
+
+if str(parsed.workmode).lower() == 'mv' or str(parsed.workmode).lower() == 'move':
+	workmode = 'mv'
+else:
+	workmode = 'cp'
+
+if verbose: print("Workmode set to", workmode)
+
+if not parsed.outputfolder:
+	outfolder = os.path.abspath('./' + os.path.splitext(datfile)[0]) # dat file without extension
+else:
+	outfolder = parsed.outputfolder
+
+
+if not os.path.exists(outfolder):
+	os.makedirs(outfolder)
+
+if not os.path.isfile(datfile):
+	print ("error! .dat file does not exist:", datfile)
+	sys.exit(1)
+
+if not os.path.exists(inputdir):
+	print("error! input folder", inputdir, "does not exist")
+	sys.exit(1)
+
+if os.path.abspath(inputdir) == os.path.abspath(outfolder):
+	print("error! input", inputdir, "and output", outputfolder, "is the same. that's probably not a good idea.")
+	sys.exit(1)
+
 
 def md5sum(filename, offset):
 	size = os.path.getsize(filename)
+	if verbose: print("md5sum", filename, str(offset) + " offset")
 	if size > large_size: # only show hash progress bar if the file is large
 		pbar_file = tqdm(total=os.path.getsize(filename))
 		pbar_file.set_description("Hashing " + os.path.basename(filename))
@@ -30,17 +73,6 @@ def md5sum(filename, offset):
 		pbar_file.close()
 	return h.hexdigest()
 
-if not os.path.exists(inputdir):
-	print ("error: roms dir: " + inputdir + ": does not exist")
-	sys.exit(1)
-
-if not os.path.isfile(datfile):
-	print ("error: dat: " + datfile + ": does not exist")
-	sys.exit(1)
-
-workdir = os.path.splitext(datfile)[0] # dat file without extension
-if not os.path.exists(workdir):
-	os.makedirs(workdir)
 
 # read dat
 with open(datfile) as df:
@@ -80,11 +112,11 @@ for path, subdir, file in os.walk(inputdir):
 		else:
 			offset = 0
 
-		h = md5sum(src, offset).lower()
+		h = md5sum(src, offset).lower() # hashing should be the slow step, so updating the progress bar after it shouldn't be terrible?
+		pbar.update(1)
 
 		if h in hashes: # dupe
 			dupes += 1
-			pbar.update(1)
 			continue
 		else:
 			hashes.append(h)
@@ -94,19 +126,24 @@ for path, subdir, file in os.walk(inputdir):
 			if h == g['rom']['@md5'].lower():
 				matches += 1
 				matched = True
-				#print ("matched:", name, "-->", g['rom']['@name'])
-				destination = os.path.join(workdir, g['rom']['@name'])
+				if verbose: print ("matched:", name, "-->", g['rom']['@name'])
+				destination = os.path.join(outfolder, g['rom']['@name'])
 				if not os.path.isfile(destination):
-					#print ("copying:", name, "-->", destination)
-					shutil.copy(src, destination)
-				#else:
-					#print ("skipping copy: already exists:", destination)
+					if workmode == 'mv':
+						if verbose: print ("moving:", name, "-->", destination)
+						shutil.move(src, destination)
+					elif workmode == 'cp':
+						if verbose: print ("copying:", name, "-->", destination)
+						shutil.copy(src, destination)
+				else:
+					if verbose: print ("skipping copy: already exists:", destination)
 				break
 
 		if matched == False:
-			#print ("no match:", name)
+			if verbose: print ("no match:", name)
 			unmatched += 1
 			unmatched_output += name + "\n"
+
 
 pbar.close()
 
@@ -118,7 +155,7 @@ for g in games:
 	h = g['rom']['@md5'].lower()
 	if h not in hashes:
 		missing += 1
-		#print ("missing:", g['rom']['@name'])
+		if verbose: print ("missing:", g['rom']['@name'])
 		missing_output += g['rom']['@name'] + "\t" + g['rom']['@md5'] + "\t" + g['rom']['@sha1'] + "\n"
 
 print ("Writing Missing.txt")
